d8a62ee819a0f050b7dad96ff30b4ace5fb10cf9
didamanu <diegopillco5@gmail.com>
2026-03-19 11:38:11 +0100

    Add files via upload

---
+++ voraces/src/main/java/com/example/Main.java
@@ -0,0 +1,20 @@
+package com.example;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Players allPlayers = new Players();
+        allPlayers.load("C:\\Users\\diego\\Desktop\\FDS\\voraces\\listado_jugadores_2026.csv");
+        allPlayers.sortByValue();
+        allPlayers.reverse();
+        allPlayers.print();
+
+        TeamMaker generator = new TeamMaker(3, 40000000, allPlayers);
+        generator.makeTeamsMode1();
+        generator.makeTeamsMode2(14);
+        generator.makeTeamsMode3();
+        generator.print();
+
+    }
+}
