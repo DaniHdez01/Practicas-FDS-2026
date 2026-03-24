0053ccb0cfac49fcc401eaf53adf24645f0483c0
DaniHdez01 <hernandeztellezdaniel05@gmail.com>
2026-03-24 13:11:27 +0100

    Cambiar nombre de la carpeta práctica 3

---
+++ Practica3/src/main/java/com/example/PlayerSorterByName.java
@@ -0,0 +1,9 @@
+package com.example;
+import java.util.Comparator;
+
+public class PlayerSorterByName implements Comparator<Player>{
+        @Override
+        public int compare(Player o1, Player o2) {
+            return o1.getName().compareToIgnoreCase(o2.getName());
+        }
+}
