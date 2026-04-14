35371f0dc3f1d01fffdad7bf3f44424ec75a65d9
DaniHdez01 <hernandeztellezdaniel05@gmail.com>
2026-04-14 09:46:18 +0200

    Acabar interfaz por terminal

--- Practica 4/BACKTRACKING_MAZE/Main.java
+++ Practica 4/BACKTRACKING_MAZE/Main.java
@@ -13,18 +13,21 @@ public class Main {
         System.out.println("PROBLEMA DEL CABALLO");
         System.out.println("Opción 1: Prueba unitaria: ");
         System.out.println("Opción 2: Generar batería de pruebas: "); 
+        System.out.println("Cualquier otra tecla que se introduzca se usará para salir del programa"); 
         int opcion = scaner.nextInt();
          Pruebas prueba = new Pruebas();  
-        if (opcion == 1){
+            if (opcion == 1){
             System.out.print("Introucir numero de filas del tablero: ");
             int filasTablero = scanner.nextInt();
             System.out.print("Introducir numero de columnas del tablero: ");
             int columnasTablero = scanner.nextInt();
             prueba.Camino_abierto(filasTablero, columnasTablero); 
             prueba.Camino_cerrado(filasTablero, columnasTablero); 
-        } else if (opcion == 2){
+            } else if (opcion == 2){
             System.out.print("Generando CSV con la batería de pruebas");
             prueba.bateria_pruebas();
+        } else {
+            break; 
         }
-    }
+}
 }
