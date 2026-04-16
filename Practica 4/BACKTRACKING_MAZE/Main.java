ddd7e3ec0095c3368c4251adab42023a631b670e
DaniHdez01 <hernandeztellezdaniel05@gmail.com>
2026-04-16 16:31:07 +0200

    Ordenar

--- Practica 4/BACKTRACKING_MAZE/Main.java
+++ Practica 4/BACKTRACKING_MAZE/Main.java
@@ -17,19 +17,19 @@ public class Main {
         int opcion = scaner.nextInt();
          Pruebas prueba = new Pruebas();  
             if (opcion == 1){
-            System.out.print("Introucir numero de filas del tablero: ");
-            int filasTablero = scanner.nextInt();
-            System.out.print("Introducir numero de columnas del tablero: ");
-            int columnasTablero = scanner.nextInt();
-            System.out.println("Opción 1: Camino abierto"); 
-            System.out.println("Opción 2: Camino cerrado"); 
-            int optdos = scanner.nextInt(); 
-            if (optdos == 1){ prueba.Camino_abierto(filasTablero, columnasTablero); }
-            else if (optdos == 2) { prueba.Camino_cerrado(filasTablero, columnasTablero);}
-            else{System.out.println("Opción no válida"); } 
+                System.out.print("Introucir numero de filas del tablero: ");
+                int filasTablero = scanner.nextInt();
+                System.out.print("Introducir numero de columnas del tablero: ");
+                int columnasTablero = scanner.nextInt();
+                System.out.println("Opción 1: Camino abierto"); 
+                System.out.println("Opción 2: Camino cerrado"); 
+                int optdos = scanner.nextInt(); 
+                if (optdos == 1){ prueba.Camino_abierto(filasTablero, columnasTablero); }
+                else if (optdos == 2) { prueba.Camino_cerrado(filasTablero, columnasTablero);}
+                else{System.out.println("Opción no válida"); } 
             } else if (opcion == 2){
-            System.out.print("Generando CSV con la batería de pruebas");
-            prueba.bateria_pruebas();
+                System.out.print("Generando CSV con la batería de pruebas");
+                prueba.bateria_pruebas();
         } else {
             System.out.println("Opción no válida"); 
             break; 
